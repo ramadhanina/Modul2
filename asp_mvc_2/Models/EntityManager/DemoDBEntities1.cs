de34b888e98eab0382f077fe8357ef79067c0b9b
Nina Aulia Ramadhani <ninshanni@gmail.com>
2017-01-19 12:21:30 +0700

    Add project files.

---
+++ asp_mvc_2/Models/EntityManager/DemoDBEntities1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Asp_mvc_2.Models.EntityManager
+{
+    internal class DemoDBEntities
+    {
+        public object SYSUserProfiles { get; internal set; }
+        public object SYSUserRoles { get; internal set; }
+        public object SYSUsers { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
